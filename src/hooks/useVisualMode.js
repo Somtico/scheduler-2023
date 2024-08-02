5710d950570749544ae5e14a4d999d69f531f390
Somto Ufondu <somtoufondu@gmail.com>
2024-08-02 14:12:16 -0600

    Implement a test to make sure the app can take in an initial mode, transition to a new mode, and revert back to the initial mode

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [history, setHistory] = useState([initial]);
+
+  function transition(newMode) {
+    setHistory((prev) => [...prev, newMode]);
+  }
+
+  function back() {
+    setHistory((prev) => [...prev.slice(0, prev.length - 1)]);
+  }
+
+  return { mode: history[history.length - 1], transition, back };
+}
