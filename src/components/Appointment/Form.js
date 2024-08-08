6887923b08b77caf7958671a916f866b65fc225f
Somto Ufondu <somtoufondu@gmail.com>
2024-08-08 15:05:41 -0600

    Create a function called validate in the body of the Form component

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -21,6 +21,15 @@ export default function Form(props) {
     props.onSave(name, interviewer);
   }
 
+  function validate() {
+    if (name === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+  
+    props.onSave(name, interviewer);
+  }
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
