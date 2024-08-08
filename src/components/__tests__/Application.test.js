3633d5c7bf81790c63fd6a561afb3d687ccccb5e
Somto Ufondu <somtoufondu@gmail.com>
2024-08-08 16:36:47 -0600

    Fire the click event on the 'Tuesday' menu button and then verify that the text 'Leopold Silvers' is in the document

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -7,7 +7,10 @@ import Application from "components/Application";
 afterEach(cleanup);
 
 it("defaults to Monday and changes the schedule when a new day is selected", () => {
-  const { findByText } = render(<Application />);
+  const { queryByText, findByText } = render(<Application />);
 
-  return findByText("Monday");
+  return findByText("Monday").then(() => {
+    fireEvent.click(queryByText("Tuesday"));
+    expect(queryByText("Leopold Silvers")).toBeInTheDocument();
+  });
 });
