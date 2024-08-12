95a6476d8425d83258c0c2e38d78dd4f83e16de2
Somto Ufondu <somtoufondu@gmail.com>
2024-08-12 05:13:28 -0600

    Implement it('loads data, cancels an interview and increases the spots remaining for Monday by 1', () => {})

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -11,7 +11,7 @@ import {
   getByText,
   queryByText,
   getByTestId,
-  prettyDOM,
+  findByAltText,
 } from "@testing-library/react";
 
 import Application from "components/Application";
@@ -67,5 +67,44 @@ describe("Application", () => {
     );
 
     expect(queryByText(day, "no spots remaining")).toBeInTheDocument();
+  });
+
+  it("loads data, cancels an interview and increases the spots remaining for Monday by 1", async () => {
+    // 1. Render the Application.
+    const { container } = render(<Application />);
+
+    // 2. Wait until the text "Archie Cohen" is displayed.
+    await findByText(container, "Archie Cohen");
+
+    // 3. Check that the DayListItem with the text "Monday" also has the text "1 spot remaining".
+    const day = getAllByTestId(container, "day").find((day) =>
+      queryByText(day, "Monday")
+    );
+
+    expect(queryByText(day, "1 spot remaining")).toBeInTheDocument();
+
+    // 4. Click the "Delete" button on the first booked appointment.
+    const appointment = getAllByTestId(container, "appointment").find(
+      (appointment) => queryByText(appointment, "Archie Cohen")
+    );
+
+    fireEvent.click(getByAltText(appointment, "Delete"));
+
+    // 5. Check that the element with the text "Are you sure you would like to delete?" is displayed.
+    expect(
+      queryByText(appointment, "Are you sure you would like to delete?")
+    ).toBeInTheDocument();
+
+    // 6. Click the "Confirm" button.
+    fireEvent.click(getByText(appointment, "Confirm"));
+
+    // 7. Check that the element with the text "Deleting" is displayed.
+    expect(queryByText(container, "Deleting")).toBeInTheDocument();
+
+    // 8. Wait until the element with the "Add" button is displayed.
+    await findByAltText(appointment, "Add");
+
+    // 9. Check that the DayListItem with the text "Monday" also has the text "2 spots remaining".
+    expect(queryByText(day, "2 spots remaining")).toBeInTheDocument();
   });
 });
